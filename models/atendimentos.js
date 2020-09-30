1da4b9a04975cc1401685ce25f394ff06e55e82e
Vinicius Brenner <vinicius_brennerr@hotmail.com>
2020-09-30 10:59:21 -0300

    quarta aula

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,43 @@
+const moment = require('moment')
 const conexao = require('../infra/conexao')
 
 class Atendimento {
-    adiciona(atendimento){
-        const sql = 'INSERT INTO Atendimentos SET ?'
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if(erro){
-                console.log(erro)
+    adiciona(atendimento, res){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+        const validacao = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data de criacao'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente de ter pelo menos 5 caracteres'
             }
-            else{
-                console.log(resultados)
-            }
-        })
+        ]
+        const erros = validacao.filter(campo => !campo.valido)
+        const existemErros = erros.length
+        if(existemErros){
+            res.status(400).json(erros)
+        }
+        else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro)
+                }
+                else{
+                    res.status(201).json(resultados)
+                }
+            })
+        }
+        
     }
 }
 
